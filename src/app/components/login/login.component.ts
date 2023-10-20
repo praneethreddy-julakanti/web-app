243bf603763f0a9d469983b6c5651225f03d9ab6
praneethreddy-julakanti <praneethreddyjulakanti@gmail.com>
2023-10-20 14:51:07 +0530

    fixed side navbar stlyes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -35,7 +35,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
 
   emailValidator(control: AbstractControl) {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const emailRegex = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     const email = control.value.toLowerCase();
   
     if (!emailRegex.test(email)) {
